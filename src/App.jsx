a69f8291de4788fbb7a67eae30a3da1915a6eb8d
thaanachot <thanachot.pongkonglad@gmail.com>
2023-08-23 19:38:51 +0700

    add: all function and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { HiX } from "react-icons/hi";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addTextToList = () => {
+    const newTodoList = [...todoList];
+    const object = {};
+    text ? (object.text = text) : null;
+    object.check = "";
+    object.text ? newTodoList.push(object) : null;
+    setTodoList(newTodoList);
+    setText("");
+  };
+
+  const addCheck = (getIndex) => {
+    const newTodoList = [...todoList];
+    if (todoList[getIndex].check === "") {
+      newTodoList[getIndex].check = "checked";
+    } else {
+      newTodoList[getIndex].check = "";
+    }
+    setTodoList(newTodoList);
+  };
+
+  const deleteList = (getIndex) => {
+    const newTodoList = todoList.filter((item, index) => {
+      return index !== getIndex;
+    });
+
+    setTodoList(newTodoList);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center mt-[25%]">
+      <div
+        className="menu bg-base-200 w-[50%] h-auto rounded-box items-center justify-around min-h-[10rem]
+      gap-[2rem] py-7"
+      >
+        <h2 className="text-4xl font-extrabold ">T O D O</h2>
+        <div className="w-[90%] flex justify-between gap-5 items-center mx-5 ">
+          <input
+            className="input input-bordered input-secondary w-[80%] text-center "
+            type="text"
+            placeholder="Type here"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+
+          <button onClick={addTextToList} className="btn btn-primary">
+            Add To Do
+          </button>
+        </div>
+        <ul className="menu bg-base-200 w-full rounded-box p-0 ">
+          {todoList.map((item, index) => {
+            return (
+              <li
+                key={index}
+                className="menu bg-base-200 w-full rounded-box flex-row items-center justify-between "
+              >
+                <h2 className="w-full justify-between text-lg font-semibold">
+                  <div className="form-control">
+                    <label className="cursor-pointer label">
+                      <input
+                        type="checkbox"
+                        checked={item.check}
+                        className="checkbox checkbox-success"
+                        readOnly
+                        onClick={() => {
+                          addCheck(index);
+                        }}
+                      />
+                    </label>
+                  </div>
+
+                  {item !== {} ? item.text : null}
+
+                  <button
+                    onClick={() => {
+                      deleteList(index);
+                    }}
+                    className="flex items-center h-7 w-7 rounded-lg bg-red-500 "
+                  >
+                    <HiX className="w-[100%]" />
+                  </button>
+                </h2>
+              </li>
+            );
+          })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
